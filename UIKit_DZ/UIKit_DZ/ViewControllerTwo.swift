3abbfcbe016a757f555d81656c89b853e57fd4a2
tixomark <tixomark@mail.ru>
2024-02-06 23:32:50 +0300

    feat(SecondScreen): add lifecycle function

--- UIKit_DZ/UIKit_DZ/ViewControllerTwo.swift
+++ UIKit_DZ/UIKit_DZ/ViewControllerTwo.swift
@@ -5,14 +5,32 @@ import UIKit
 
 /// Second screen
 class ViewControllerTwo: UIViewController {
-    // MARK: = Public Properties
-
-    var aaa = String()
-
-    // MARK: - Public Methods
+    // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(aaa)
+        print(#function)
+        hello()
+    }
+
+    override func loadView() {
+        super.loadView()
+        print(#function)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(#function)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print(#function)
+    }
+
+    // MARK: - Public Methods
+
+    func hello() {
+        print("hi")
     }
 }
