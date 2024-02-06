2a9cf459b2b37bdefd6c86d6c89c42ccef5d042f
tixomark <tixomark@mail.ru>
2024-02-06 22:30:01 +0300

    feat(secondScreen): implement second screen

---
+++ UIKit_DZ/UIKit_DZ/ViewControllerTwo.swift
@@ -0,0 +1,18 @@
+// ViewControllerTwo.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Second screen
+class ViewControllerTwo: UIViewController {
+    // MARK: = Public Properties
+
+    var aaa = String()
+
+    // MARK: - Public Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(aaa)
+    }
+}
