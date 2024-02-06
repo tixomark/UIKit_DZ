b2752064dec7d89bfa89483b987b488565b01809
tixomark <tixomark@mail.ru>
2024-02-06 21:03:29 +0300

    feat(app): create base project

---
+++ UIKit_DZ/UIKit_DZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_DZ
+//
+//  Created by Tixon Markin on 06.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
