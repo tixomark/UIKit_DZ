482e2167f460a656a8c61c5c9f161e3157857fda
tixomark <tixomark@mail.ru>
2024-02-07 14:38:02 +0300

    feat(app): Add MainViewController as root to window

---
+++ UIKit_DZ/UIKit_DZ/MainViewController.swift
@@ -0,0 +1,14 @@
+// MainViewController.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Main screen view
+final class MainViewController: UIViewController {
+    // MARK: - Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
