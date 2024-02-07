390f2ed731559a16a6469a3b7a1ee4fef9b38fd2
tixomark <tixomark@mail.ru>
2024-02-08 01:03:19 +0300

    feat(SceneDelegate): Set up window

--- UIKit_DZ/UIKit_DZ/SceneDelegate.swift
+++ UIKit_DZ/UIKit_DZ/SceneDelegate.swift
@@ -5,4 +5,21 @@ import UIKit
 
 /// Scene delegate
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    // MARK: - Public Properties
+
+    var window: UIWindow?
+
+    // MARK: - Life Cycle
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+    }
 }
