5944fe5ce55d3cae9d9cce60d39a55a0f8251d07
tixomark <tixomark@mail.ru>
2024-02-08 14:42:35 +0300

    feat(ReverterModel): Created and set up model

--- UIKit_DZ/UIKit_DZ/MainScreen/ReverterModel.swift
+++ UIKit_DZ/UIKit_DZ/MainScreen/ReverterModel.swift
@@ -4,4 +4,24 @@
 import Foundation
 
 /// Model for reverting strings
-final class ReverterModel {}
+final class ReverterModel {
+    // MARK: - Public Properties
+
+    var reversedWord: String {
+        var result = String(initialWord.reversed())
+        if let firstLetter = initialWord.first, firstLetter.isUppercase {
+            result = result.capitalized
+        }
+        return result
+    }
+
+    // MARK: - Private Properties
+
+    private(set) var initialWord: String
+
+    // MARK: - Life Cycle
+
+    init(initialWord: String) {
+        self.initialWord = initialWord
+    }
+}
