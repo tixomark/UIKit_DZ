966f71ca81c057813ea8db0c3f4a3d08ce6b1c7f
tixomark <tixomark@mail.ru>
2024-02-08 14:32:38 +0300

    feat(MainVC): Add enterYourWordAlert and corresponding logic

--- UIKit_DZ/UIKit_DZ/MainScreen/MainViewController.swift
+++ UIKit_DZ/UIKit_DZ/MainScreen/MainViewController.swift
@@ -5,6 +5,8 @@ import UIKit
 
 /// Main screen view
 final class MainViewController: UIViewController {
+    // MARK: - Constants
+
     let startButton: UIButton = {
         let button = UIButton()
         button.layer.cornerRadius = 12
@@ -14,6 +16,12 @@ final class MainViewController: UIViewController {
         button.titleLabel?.font = UIFont(name: "Verdana-Bold", size: 16)
         return button
     }()
+
+    // MARK: - Private Properties
+
+    private var enterYourWordAlertOkAktion: UIAlertAction?
+
+    // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +40,50 @@ final class MainViewController: UIViewController {
         startButton.frame = CGRect(origin: startButtonOrigin, size: startButtonSize)
     }
 
+    // MARK: - Private Methods
+
     private func setUpUI() {
         view.backgroundColor = .systemBackground
 
+        startButton.addTarget(self, action: #selector(didTapStartButton(_:)), for: .touchUpInside)
         view.addSubview(startButton)
+    }
+
+    private func showEnterYourWordAlert() {
+        let alert = UIAlertController(
+            title: "Введите ваше слово",
+            message: nil,
+            preferredStyle: .alert
+        )
+        alert.addTextField { [unowned self] textField in
+            textField.placeholder = "Введите слово"
+            textField.addTarget(self, action: #selector(textDidChangeIn(_:)), for: .editingChanged)
+        }
+
+        let cancelAction = UIAlertAction(title: "Отмена", style: .default)
+        alert.addAction(cancelAction)
+
+        enterYourWordAlertOkAktion = UIAlertAction(title: "Ок", style: .cancel) { _ in
+            print(alert.textFields?.first?.text ?? "")
+        }
+        enterYourWordAlertOkAktion?.isEnabled = false
+        if let enterYourWordAlertOkAktion {
+            alert.addAction(enterYourWordAlertOkAktion)
+        }
+
+        present(alert, animated: true)
+    }
+
+    @objc private func didTapStartButton(_ sender: UIButton) {
+        showEnterYourWordAlert()
+    }
+
+    @objc private func textDidChangeIn(_ sender: UITextField) {
+        guard let text = sender.text, !text.isEmpty else {
+            enterYourWordAlertOkAktion?.isEnabled = false
+            return
+        }
+        print(text)
+        enterYourWordAlertOkAktion?.isEnabled = true
     }
 }
